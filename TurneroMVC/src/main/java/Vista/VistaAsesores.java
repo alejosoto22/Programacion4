bbcf84c28cbecabb52de2878d47058362fcc9691
alejandroSoto <alejandro.soto@utp.edu.co>
2019-06-28 01:01:35 -0500

    28/062019

--- TurneroMVC/src/main/java/Vista/VistaAsesores.java
+++ TurneroMVC/src/main/java/Vista/VistaAsesores.java
@@ -47,7 +47,9 @@ public class VistaAsesores extends JFrame implements InterfazVista{
 	 * Create the frame.
 	 */
 	public VistaAsesores() {
+		setResizable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(6);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
